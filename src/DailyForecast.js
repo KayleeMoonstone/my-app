6feb30133b05b0002a696cbe07394fe8ab323981
KayleeMoonstone <134209097+KayleeMoonstone@users.noreply.github.com>
2023-08-23 13:32:47 +0200

    Updated the DailyForecast.js file

--- src/DailyForecast.js
+++ src/DailyForecast.js
@@ -8,21 +8,23 @@ export default function DailyForecast({ weatherData }) {
 
   return (
     <div className="dailyForecast">
-      <div>
-        <span className="title">Wind:</span> &nbsp;
-        <span className="value">{weatherData.windSpeed} m/s</span>
-      </div>
-      <div>
-        <span className="title">Humidity:</span> &nbsp;
-        <span className="value">{weatherData.humidity} %</span>
-      </div>
-      <div>
-        <span className="title">Cloud Cover:</span> &nbsp;
-        <span className="value">{weatherData.cloudCover} %</span>
-      </div>
-      <div>
-        <span className="title">Rainfall:</span> &nbsp;
-        <span className="value">{weatherData.rainFall} %</span>
+      <div className="row">
+        <div className="col">
+          <span className="title">Wind:</span> &nbsp;
+          <span className="value">{weatherData.windSpeed} m/s</span>
+        </div>
+        <div>
+          <span className="title">Humidity:</span> &nbsp;
+          <span className="value">{weatherData.humidity} %</span>
+        </div>
+        <div>
+          <span className="title">Cloud Cover:</span> &nbsp;
+          <span className="value">{weatherData.cloudCover} %</span>
+        </div>
+        <div>
+          <span className="title">Rainfall:</span> &nbsp;
+          <span className="value">{weatherData.rainFall} %</span>
+        </div>
       </div>
     </div>
   );
